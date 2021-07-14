4aff6ca56dbaf0293c96c9b23d8a651c69fab6aa
Wallace Ferreira <mr.wallaceferreira@gmail.com>
2021-07-14 19:57:32 -0300

    definir movimentos da cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
-function criarBG(){
-    context.fillStyle = lightgreen;
-    context.fillReact(0, 16 * box, 16 * box);
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBG()
+let direction = "right";
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillReact(0, 0, 16 * box, 16 * box);
+}
+
+function crirCobrinha(){
+    for(i=0; i < snake.langh; i++){
+        context.fillStyle = "green";
+        context.fillReact(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function inicarJogo(){
+    criarBG();
+    crirCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].Y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = serInterval(inicarJogo, 100);
